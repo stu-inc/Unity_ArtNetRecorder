792479c837f616ed77113c04a83407594d0914c7
Kodai Takao (MSI GS65) <kodai100@gmail.com>
2023-03-08 18:22:50 +0900

    implement signal from dmx channel component

--- Assets/Scripts/View/UI/List/DmxChannelListComponentUI.cs
+++ Assets/Scripts/View/UI/List/DmxChannelListComponentUI.cs
@@ -15,6 +15,12 @@ namespace com.kodai100.ArtNetApp.View
         [SerializeField] private TMP_InputField _channelValueInputField;
         [SerializeField] private Slider _slider;
         [SerializeField] private UnityEngine.UI.Button _deleteButton;
+
+        private Subject<(Guid, int)> _onChannelIndexChanged = new();
+        public IObservable<(Guid, int)> OnChannelIndexChanged => _onChannelIndexChanged;
+
+        private Subject<(Guid, int)> _onChannelValueChanged = new();
+        public IObservable<(Guid, int)> OnChannelValueChanged => _onChannelValueChanged;
         
         public IObservable<Guid> OnDeleteButtonClicked => _deleteButton.OnClickAsObservable().Select(_ => _data.Guid);
 
@@ -28,6 +34,47 @@ namespace com.kodai100.ArtNetApp.View
             _slider.value = data.ChannelValue;
             _channelValueInputField.text = data.ChannelValue.ToString();
 
+            _channelInputField.onEndEdit.AsObservable().Subscribe(text =>
+            {
+                if (int.TryParse(text, out var result))
+                {
+                    _onChannelIndexChanged.OnNext((_data.Guid, result));
+                }
+                else
+                {
+                    _onChannelIndexChanged.OnNext((_data.Guid, 0));
+                    _channelInputField.SetTextWithoutNotify(0.ToString());
+                }
+            }).AddTo(this);
+
+            _channelValueInputField.onEndEdit.AsObservable().Subscribe(text =>
+            {
+                if (int.TryParse(text, out var result))
+                {
+                    _onChannelValueChanged.OnNext((_data.Guid, result));
+                }
+                else
+                {
+                    _onChannelValueChanged.OnNext((_data.Guid, 0));
+                    _channelValueInputField.SetTextWithoutNotify(0.ToString());
+                }
+            }).AddTo(this);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">0-255</param>
+        public void SetChannelValue(int value)
+        {
+            _slider.value = value;
+            _channelValueInputField.text = value.ToString();
+        }
+
+        public void SetChannelIndex(int value)
+        {
+            _channelInputField.text = value.ToString();
         }
     }
     
--- Assets/Scripts/View/UI/List/DmxChannelListUI.cs
+++ Assets/Scripts/View/UI/List/DmxChannelListUI.cs
@@ -12,6 +12,11 @@ namespace com.kodai100.ArtNetApp.View
         [SerializeField] private DmxChannelListComponentUI _componentViewPrefab;
         
         private Subject<Guid> _onComponentDeleted = new();
+        private Subject<(Guid, int)> _onChannelIndexChanged = new();
+        private Subject<(Guid, int)> _onChannelValueChanged = new();
+        
+        public IObservable<(Guid, int)> OnChannelIndexChanged => _onChannelIndexChanged;
+        public IObservable<(Guid, int)> OnChannelValueChanged => _onChannelValueChanged;
         public IObservable<Guid> OnComponentDeleted => _onComponentDeleted;
         
         protected override ReorderableListComponentView<DmxChannelEntity> ReorderableListComponentViewPrefab =>
@@ -29,8 +34,50 @@ namespace com.kodai100.ArtNetApp.View
                     _onComponentDeleted.OnNext(guid);
                 })
                 .AddTo(disposables);
+
+                converted.OnChannelIndexChanged.Subscribe(v =>
+                {
+                    _onChannelIndexChanged.OnNext(v);
+                }).AddTo(disposables);
+
+                converted.OnChannelValueChanged.Subscribe(v =>
+                {
+                    _onChannelValueChanged.OnNext(v);
+                }).AddTo(this);
             }
         }
-        
+
+        public void SetChannelIndex(Guid id, int value)
+        {
+            // TODO: 値が変更されるたびに検索が走るので、もっといい方法があるはず
+            // → キャッシュして、GUIDが変わってない時はキャッシュから取得するようにする
+            
+            var target = _listComponents.FirstOrDefault(x => x.Data.Guid == id);
+            if (target == null) return;
+            
+            var converted = target as DmxChannelListComponentUI;
+            if (converted == null) return;
+            
+            converted.SetChannelIndex(value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id">Target component's GUID</param>
+        /// <param name="value">0-255 dmx value</param>
+        public void SetChannelValue(Guid id, int value)
+        {
+            // TODO: 値が変更されるたびに検索が走るので、もっといい方法があるはず
+            // → キャッシュして、GUIDが変わってない時はキャッシュから取得するようにする
+            
+            var target = _listComponents.FirstOrDefault(x => x.Data.Guid == id);
+            if (target == null) return;
+            
+            var converted = target as DmxChannelListComponentUI;
+            if (converted == null) return;
+            
+            converted.SetChannelValue(value);
+        }
     }
 }
